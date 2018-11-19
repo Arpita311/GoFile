c7b77a113af4de8f8c5632b40039d1a08854bab8
Arpita311 <arpita.baheti@click2cloud.net>
2018-11-19 12:19:24 +0530

    Subtraction code is added

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ func main() {
 
 		return no1+no2
 }(num1,num2)
-fmt.Printf("Thw adtition of two numbers %d + %d is  %d",num1,num2,c)
+fmt.Printf("The ADDITION of two numbers %d + %d is  %d",num1,num2,c)
+
+
+
+	d:= func(n1,n2 int)int{
+		return n2-n1
+	}(num1,num2)
+	fmt.Printf("\n \n The SUBTRACTION of two numbers %d - %d is  %d",num2,num1,d)
 }
